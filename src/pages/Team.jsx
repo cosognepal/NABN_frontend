52ee8f598ddc33a286cb8d1efd303ddcbe115be4
Amogh Manral <amogh.manral@duke.edu>
2025-03-21 15:43:17 -0400

    team members collection type

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -5,7 +5,7 @@ import useFetch from "../hooks/useFetch.js";
 import "./Team.css";
 
 function Team() {
-  const { loading, error, data } = useFetch("https://charming-trust-6afb776746.strapiapp.com/api/team?populate[pagecontent][populate]=*");
+  const { loading, error, data } = useFetch("http://charming-trust-6afb776746.strapiapp.com/api/teammembers?populate=*");
 
   useEffect(() => {
     // Set page title when component mounts
@@ -28,35 +28,8 @@ function Team() {
     </div>
   );
 
-  // Extract page content from the response
-  const pageContent = data?.data?.pagecontent || [];
-
-  // Process the content to group team members
-  const teamMembers = [];
-  let currentMember = {};
-
-  pageContent.forEach(content => {
-    if (content.__component === "components.subtitle") {
-      // If we already have a member with data, add it to our array
-      if (currentMember.name) {
-        teamMembers.push({...currentMember});
-      }
-      // Start a new member
-      currentMember = { name: content.subtitle };
-    } else if (content.__component === "components.image") {
-      // Add image to current member - use the full URL directly
-      const imageUrl = content.image?.url || "/placeholder-avatar.png";
-      currentMember.image = imageUrl;
-    } else if (content.__component === "components.description") {
-      // Add description to current member
-      currentMember.description = content.description;
-    }
-  });
-
-  // Don't forget to add the last member
-  if (currentMember.name) {
-    teamMembers.push({...currentMember});
-  }
+  // Get the team members data from the response
+  const teamMembers = data?.data || [];
 
   return (
     <div className="team-page">
@@ -65,21 +38,25 @@ function Team() {
         <h1 className="team-title">Our Team</h1>
         
         <div className="team-members">
-          {teamMembers.map((member, index) => (
-            <div key={index} className="team-member">
-              <div className="member-image-container">
-                <img 
-                  src={member.image} 
-                  alt={`${member.name} portrait`} 
-                  className="member-image"
-                />
+          {teamMembers.length > 0 ? (
+            teamMembers.map(member => (
+              <div key={member.id} className="team-member">
+                <div className="member-image-container">
+                  <img 
+                    src={member.image?.url || "/placeholder-avatar.png"} 
+                    alt={`${member.name} portrait`} 
+                    className="member-image"
+                  />
+                </div>
+                <div className="member-details">
+                  <h2 className="member-name">{member.name}</h2>
+                  <p className="member-description">{member.description}</p>
+                </div>
               </div>
-              <div className="member-details">
-                <h2 className="member-name">{member.name}</h2>
-                <p className="member-description">{member.description}</p>
-              </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No team members found</p>
+          )}
         </div>
       </div>
       <Footer />
