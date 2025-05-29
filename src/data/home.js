e3fac28896964e447a8ff6ec42847f9fa1c93c1b
Ghanashyam45 <ghanashyam.bishwakarma2024@nalc.edu.np>
2025-05-29 15:05:57 +0545

    replace youth with adolescent

--- src/data/home.js
+++ src/data/home.js
@@ -6,7 +6,7 @@ import Icon3 from "../assets/icons/third.png";
 const data = {
   heroSection: {
     title: "National Adolescent Boys Network Nepal",
-    subtitle: "Empowering Nepal's youth",
+    subtitle: "Empowering Nepal's adolescent",
     CTAbutton: "Get Involved",
     CTAbuttonlink: "https://www.facebook.com/nabn.nepal/",
     image: {
@@ -22,7 +22,7 @@ const data = {
     mission2subtitle:
       "Ending gender-based violence and promoting equality for all adolescents",
     icon2: { url: Icon2 },
-    mission3: "Youth Advocacy",
+    mission3: "Adolescent Advocacy",
     mission3subtitle:
       "Empowering adolescents across all seven provinces to speak up for change",
     icon3: { url: Icon3 },
@@ -38,7 +38,7 @@ const data = {
   },
   ctaSection: {
     CTAtitle: "Ready to Make a Difference?",
-    CTAsubtitle: "Join us in empowering Nepal's youth",
+    CTAsubtitle: "Join us in empowering Nepal's Adolescent",
     CTAbutton: "Learn More",
     CTAbuttonLink: "/about",
   },
